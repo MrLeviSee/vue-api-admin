6050f1f008999871e4e84cc3e6b93702ede8af2b
a木木 <mumuwj@com>
2025-03-19 22:25:37 +0800

    项目初始化

---
+++ mock/login.ts
@@ -0,0 +1,43 @@
+// 根据角色动态生成路由
+import { defineFakeRoute } from "vite-plugin-fake-server/client";
+
+export default defineFakeRoute([
+  {
+    url: "/login",
+    method: "post",
+    response: ({ body }) => {
+      if (body.username === "admin") {
+        return {
+          success: true,
+          data: {
+            avatar:
+              "https://q.qlogo.cn/qqapp/102711861/34D763DDCF1093A546D9189AC48A105B/640",
+            username: "admin",
+            nickname: "晚风",
+            // 一个用户可能有多个角色
+            roles: ["admin"],
+            // 按钮级别权限
+            permissions: ["*:*:*"],
+            accessToken: "eyJhbGciOiJIUzUxMiJ9.admin",
+            refresh_token: "eyJhbGciOiJIUzUxMiJ9.adminRefresh",
+            expires: "2030/10/30 00:00:00"
+          }
+        };
+      } else {
+        return {
+          success: true,
+          data: {
+            avatar: "https://avatars.githubusercontent.com/u/52823142",
+            username: "common",
+            nickname: "小林",
+            roles: ["common"],
+            permissions: ["permission:btn:add", "permission:btn:edit"],
+            accessToken: "eyJhbGciOiJIUzUxMiJ9.common",
+            refreshToken: "eyJhbGciOiJIUzUxMiJ9.commonRefresh",
+            expires: "2030/10/30 00:00:00"
+          }
+        };
+      }
+    }
+  }
+]);
---
+++ src/api/system/menu.ts
@@ -0,0 +1,52 @@
+import { http } from "@/utils/http";
+import type { ResultDetail } from "@/api/types";
+
+/** 获取菜单列表，用于动态菜单 */
+export const getAsyncRoutes = () => {
+  console.log("getAsyncRoutes");
+  return http.request<ResultDetail>("get", "/api/system/menu", {});
+};
+
+/** 获取菜单列表，用于菜单表格 */
+export const getRoutesData = (data?: object) => {
+  return http.request<ResultDetail>("get", "/api/system/menu/data", {
+    params: data
+  });
+};
+
+/** 菜单添加 */
+export const createMenuApi = (data?: object) => {
+  return http.request<ResultDetail>("post", "/api/system/menu/add", {
+    data: data
+  });
+};
+
+/** 菜单删除 */
+export const deleteMenuApi = Id => {
+  const url = `/api/system/menu/${Id}`;
+  return http.request<ResultDetail>("delete", url);
+};
+
+/** 菜单更新状态 */
+export const UpdateMenu_status = Id => {
+  const url = `/api/system/menu/${Id}/status`;
+  return http.request<ResultDetail>("put", url);
+};
+
+/** 批量删除菜单 */
+export const manyDeleteMenuApi = (data?: object) => {
+  return http.request<ResultDetail>(
+    "post",
+    "/api/system/menu/del/batch_remove",
+    {
+      data
+    }
+  );
+};
+
+/** 更新菜单数据 */
+export const UpdateMenu = (Id: number, data?: object | string) => {
+  return http.request<ResultDetail>("put", `/api/system/menu/${Id}/info`, {
+    data
+  });
+};
